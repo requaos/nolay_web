e34ed5e943308beff9604a841fea53f6f2deb1d4
Neil Skinner <neils@knowbe4.com>
2021-11-03 22:44:47 -0400

    use vue-router to merge play and blank

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <v-app id="app">
     <Header></Header>
-    <Body></Body>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
 import 'vuetify/dist/vuetify.min.css'
 import Header from './components/header.vue'
-import Body from './components/body.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Body
+    Header
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import Body from './components/body.vue'
+import Reading from './components/reading.vue'
 import Vuetify from 'vuetify'
 import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 
+const routes = [
+  { path: '/reader', component: Reading },
+  { path: '/', component: Body }
+]
+
+const router = new VueRouter({
+  routes: routes
+})
+
 import './sass/main.css'
 new Vue({
+  router: router,
   render: h => h(App),
-  vuetify: vuetify,
-  
+  vuetify: vuetify
 }).$mount('#app')
